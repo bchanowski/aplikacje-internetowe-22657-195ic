48e6517606921c84bd6a23e1752eabee93d48177
acha <22657@edu.amw.gdynia.pl>
2021-10-30 20:40:09 +0200

    Dodanie rejestracji i loginu

--- mysite/settings.py
+++ mysite/settings.py
@@ -132,3 +132,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'bchanowskiTest@gmail.com'
+EMAIL_HOST_PASSWORD = 'testhaslo2'
+EMAIL_PORT = 576
