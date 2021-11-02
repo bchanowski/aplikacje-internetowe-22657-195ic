e6b2a93b64663c53d5e2c2b8d2df5877926252e3
acha <22657@edu.amw.gdynia.pl>
2021-11-02 16:42:43 +0100

    Naprawa django w heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -139,3 +140,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'revidelol@gmail.com'
 EMAIL_HOST_PASSWORD = 'wxsqcnvmckowjfwx'
 EMAIL_PORT = 576
+
+django_heroku.settings(locals())
